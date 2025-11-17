e781b29ce417e03ab3753bb23a5dc9656ef3e47a
garygoodly <garygoodly1@gmail.com>
2025-11-17 20:00:53 +0800

    23 finished

---
+++ dmsxl/Tree/23_669.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def trimBST(self, root, low, high):
+        """
+        :type root: Optional[TreeNode]
+        :type low: int
+        :type high: int
+        :rtype: Optional[TreeNode]
+        """
+        if root == None:
+            return None
+
+        if root.val < low:
+            right = self.trimBST(root.right, low, high)
+            return right
+        
+        elif high < root.val:
+            left = self.trimBST(root.left, low, high)
+            return left
+        
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
+
+        return root
