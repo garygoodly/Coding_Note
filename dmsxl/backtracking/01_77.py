91468568a43c20db5710015ac640ef1de198ca3c
garygoodly <garygoodly1@gmail.com>
2025-11-20 20:47:30 +0800

    bt 1 finished.

--- dmsxl/backtracking/01_77.py
+++ dmsxl/backtracking/01_77.py
@@ -1,8 +1,16 @@
 class Solution(object):
-    def backtracking(self, ans, n, k):
-        if len(ans[0]) == k:
+    def backtracking(self, ans, path, n, k):
+        if len(path) == k:
+            ans.append(path)
             return
-        for i in range(1, n - k - ans[0]):
+        
+        if len(path) == 0:
+            for i in range(1, n - k + 2):
+                self.backtracking(ans, [i], n, k)
+        else:
+            for i in range(path[-1] + 1, n - k + len(path) + 2):
+                self.backtracking(ans, path + [i], n, k)
+        return
             
     def combine(self, n, k):
         """
@@ -10,4 +18,49 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
+        ans = []
+        self.backtracking(ans, [], n, k)
+        return ans
+    
+
+class Solution(object):
+    def backtracking(self, ans, path, n, k, startIdx):
+        if len(path) == k:
+            ans.append(path[:])
+            return
         
+        for i in range(startIdx, n + 1):
+            path.append(i)
+            self.backtracking(ans, path, n, k, i + 1)
+            path.pop()
+            
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        self.backtracking(ans, [], n, k, 1)
+        return ans
+    
+class Solution(object):
+    def backtracking(self, ans, path, n, k, startIdx):
+        if len(path) == k:
+            ans.append(path[:])
+            return
+        
+        for i in range(startIdx, n - k + len(path) + 2):
+            path.append(i)
+            self.backtracking(ans, path, n, k, i + 1)
+            path.pop()
+            
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        self.backtracking(ans, [], n, k, 1)
+        return ans
