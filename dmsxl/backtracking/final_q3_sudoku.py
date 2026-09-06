a8f3f583260b314faf69c68d14016d31ad992cb0
garygoodly <garygoodly1@gmail.com>
2026-09-06 12:52:03 +0800

    Finish BT

--- dmsxl/backtracking/final_q3_sudoku.py
+++ dmsxl/backtracking/final_q3_sudoku.py
@@ -1,31 +1,66 @@
 class Solution(object):
+
     def __init__(self):
-        self.board = None
-        self.board_fix = [[0] * 9 for _ in range(9)]
-        self.row_visit = [[0] * 10 for _ in range(9)]
-        self.col_visit = [[0] * 10 for _ in range(9)]
-        self.block_visit = [[0] * 10 for _ in range(9)]
+        self.row_visit = [[False] * 10 for _ in range(9)]
+        self.col_visit = [[False] * 10 for _ in range(9)]
+        self.block_visit = [[False] * 10 for _ in range(9)]
 
     @staticmethod
-    def map(row, col):
+    def get_block(row, col):
         return 3 * (row // 3) + col // 3
 
-    def bt(self, row, col):
+    def bt(self, idx, board):
 
+        if idx == 81:
+            return True
 
+        row = idx // 9
+        col = idx % 9
+
+        # Already filled
+        if board[row][col] != '.':
+            return self.bt(idx + 1, board)
+
+        block = self.get_block(row, col)
+
+        for num in range(1, 10):
+
+            if (self.row_visit[row][num]
+                    or self.col_visit[col][num]
+                    or self.block_visit[block][num]):
+                continue
+
+            # choose
+            board[row][col] = str(num)
+
+            self.row_visit[row][num] = True
+            self.col_visit[col][num] = True
+            self.block_visit[block][num] = True
+
+            # recurse
+            if self.bt(idx + 1, board):
+                return True
+
+            # undo
+            board[row][col] = '.'
+
+            self.row_visit[row][num] = False
+            self.col_visit[col][num] = False
+            self.block_visit[block][num] = False
+
+        return False
 
     def solveSudoku(self, board):
-        self.board = board
-        self.board_fix = [[1 if self.board[i][j] != '.' else 0 for j in range(9)] for i in range(9)]
 
         for row in range(9):
             for col in range(9):
-                if self.board[row][col] != '.':
-                    tmp = int(self.board[row][col])
-                    self.row_visit[row][tmp] = 1
-                    self.col_visit[col][tmp] = 1
-                    self.block_visit[self.map(row, col)][tmp] = 1
-        #
-        # print(self.row_visit)
-        # print(self.col_visit)
-        # print(self.block_visit)
+
+                if board[row][col] != '.':
+                    num = int(board[row][col])
+                    block = self.get_block(row, col)
+
+                    self.row_visit[row][num] = True
+                    self.col_visit[col][num] = True
+                    self.block_visit[block][num] = True
+
+        self.bt(0, board)
